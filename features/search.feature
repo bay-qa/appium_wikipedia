1338fb281a7adb7b33688043774e29e6927faa7a
Aliaksei Piatrouski <Aliaksei@Piatrouski>
2015-10-04 10:03:14 -0700

    day2

--- features/search.feature
+++ features/search.feature
@@ -1,7 +1,27 @@
+@search
 Feature: Wikipedia search
-
 
   Scenario: As Wikipedia user, I would like to perform simple search
     When I tap on skip button
+    Then I land on Home screen
+    And I tap on Search button
+    Then I land on Search screen
+    And I type "test automation"
+    Then I see that 1st search result include "test automation"
+    When I tap on 1st result
+    And I verify that article title include "test automation"
 
+  Scenario Outline: As Wikipedia user, I would like to perform simple search
+    When I tap on skip button
+    Then I land on Home screen
+    And I tap on Search button
+    Then I land on Search screen
+    And I type "<subject>"
+    Then I see that 1st search result include "<subject>"
+    When I tap on 1st result
+    And I verify that article title include "<search_result>"
 
+    Examples: search criteria
+      |subject|search_result|
+      |test automation | test automation |
+      |Selenium Web Driver | selenium |
--- features/step_definitions/common_steps.rb
+++ features/step_definitions/common_steps.rb
@@ -2,3 +2,54 @@ When(/^I tap on skip button$/) do
   find_element(:name, "Skip").click
 end
 
+Then(/^I land on Home screen$/) do
+  result = find_element(:name, "Home").attribute("value")
+  expect(result).to be == 1
+end
+
+And(/^I tap on Search button$/) do
+  find_element(:name, "Search").click
+end
+
+Then(/^I land on Search screen$/) do
+  # result = find_element(:name, "Search").attribute("value")
+  # expect(result).to be == 1
+  expect(find_element(:class_name, "UIASearchBar").text).to be == "Search Wikipedia"
+end
+
+
+And(/^I type "([^"]*)"$/) do |search_term|
+  find_element(:class_name, "UIASearchBar").clear
+  find_element(:class_name, "UIASearchBar").send_keys search_term
+end
+
+Then(/^I see that 1st search result include "([^"]*)"$/) do |expected_result|
+  element = find_element(:xpath, "//UIAApplication[1]/UIAWindow[1]/UIACollectionView[1]/UIACollectionCell[1]/UIAStaticText[1]")
+  expect(element.text.downcase).to include expected_result
+end
+
+
+When(/^I tap on (\d+)(?:st|nd|rd|th)? result$/) do |index|
+  find_element(:xpath, "//UIAApplication[1]/UIAWindow[1]/UIACollectionView[1]/UIACollectionCell[#{index}]/UIAStaticText[1]").click
+end
+
+
+And(/^I verify that article title include "([^"]*)"$/) do |expected_result|
+  element = find_element(:xpath, "//UIAApplication[1]/UIAWindow[1]/UIATableView[1]/UIAStaticText[1]")
+  expect(element.text.downcase).to be == expected_result
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
